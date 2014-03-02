4aa37d49d0153924f13cc2e91633828cba31bdf7
White Rabbit <rabbit@archer.local>
2014-03-02 17:55:03 +0700

    refactor and remove leading number

--- berlin_clock.rb
+++ berlin_clock.rb
@@ -5,34 +5,35 @@ class BerlinClock
   end
 
   def display
+    "#{get_second} #{get_hour} #{get_minute}"
+  end
+
+  private
+  def get_second
     second = @time[6..7].to_i
+    sec = (second % 2 == 1 ? "O" : "Y")
+  end
+
+  def get_minute
     minute = @time[3..4].to_i
-    hour = @time[0..1].to_i
-    min2_display = "5OOOO"
-
-    case second % 2
-      when 0
-        second_display = "1Y"
-      when 1 
-        second_display = "1O"
-    end
-
     min_div_five = minute / 5
     min_mod_five = minute % 5
+    min1 = ""
+    (1..(min_div_five)).each do |i|
+      min1 += i % 3 != 0 ? "Y" : "R"
+    end
+    min1 += "O" * (11 - min_div_five)
+    min2 = "Y" * (min_mod_five) + "O" * (4 - min_mod_five) 
+    "#{min1} #{min2}"
+  end
+
+  def get_hour
+    hour = @time[0..1].to_i
     hour_div_five = hour / 5
     hour_mod_five = hour % 5
-
-    min2_display = " 5" + "Y" * (min_mod_five) + "O" * (4 - min_mod_five) 
-    hour2_display = " 2" + "Y" * (hour_div_five) + "O" * (4 - hour_div_five) 
-
-    min1_display = " 4"
-    (1..(min_div_five)).each do |i|
-      min1_display += i % 3 != 0 ? "Y" : "R"
-    end
-
-    min1_display += "O" * (11 - min_div_five)
-    hour1_display = " 3" + "Y" * (hour_mod_five) + "O" * (4 - hour_mod_five) 
-    second_display + hour2_display + hour1_display + min1_display + min2_display 
+    hour1 = "Y" * (hour_div_five) + "O" * (4 - hour_div_five) 
+    hour2 = "Y" * (hour_mod_five) + "O" * (4 - hour_mod_five) 
+    "#{hour1} #{hour2}"
   end
 
 end
--- spec/berlin_clock_spec.rb
+++ spec/berlin_clock_spec.rb
@@ -4,73 +4,73 @@ describe "Berlin Clock" do
 
   let (:berlin_clock) { BerlinClock.new }
 
-  it "should return 1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO when input is 00:00:00" do
+  it "should return Y OOOO OOOO OOOOOOOOOOO OOOO when input is 00:00:00" do
     berlin_clock.setTime "00:00:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO OOOO"
   end
 
-  it "should return 1O 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO when input is 00:00:01" do
+  it "should return O OOOO OOOO OOOOOOOOOOO OOOO when input is 00:00:01" do
     berlin_clock.setTime "00:00:01"
-    expect berlin_clock.display.should eq "1O 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO"
+    expect berlin_clock.display.should eq "O OOOO OOOO OOOOOOOOOOO OOOO"
   end
 
-  it "should return 1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5YOOO when input is 00:01:00" do
+  it "should return Y OOOO OOOO OOOOOOOOOOO YOOO when input is 00:01:00" do
     berlin_clock.setTime "00:01:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5YOOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO YOOO"
   end
 
-  it "should return 1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5YYYY when input is 00:04:00" do
+  it "should return Y OOOO OOOO OOOOOOOOOOO YYYY when input is 00:04:00" do
     berlin_clock.setTime "00:04:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5YYYY"
+    expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO YYYY"
   end
   
-  it "should return 1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5YYOO when input is 00:02:00" do
+  it "should return Y OOOO OOOO OOOOOOOOOOO YYOO when input is 00:02:00" do
     berlin_clock.setTime "00:02:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5YYOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO YYOO"
   end
 
-  it "should return 1Y 2OOOO 3OOOO 4YOOOOOOOOOO 5OOOO when input is 00:05:00" do
+  it "should return Y OOOO OOOO YOOOOOOOOOO OOOO when input is 00:05:00" do
     berlin_clock.setTime "00:05:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4YOOOOOOOOOO 5OOOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO YOOOOOOOOOO OOOO"
   end
 
-  it "should return 1Y 2OOOO 3OOOO 4YOOOOOOOOOO 5YYOO when input is 00:07:00" do
+  it "should return Y OOOO OOOO YOOOOOOOOOO YYOO when input is 00:07:00" do
     berlin_clock.setTime "00:07:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4YOOOOOOOOOO 5YYOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO YOOOOOOOOOO YYOO"
   end
 
-  it "should return 1Y 2OOOO 3OOOO 4YYOOOOOOOOO 5OOOO when input is 00:10:00" do
+  it "should return Y OOOO OOOO YYOOOOOOOOO OOOO when input is 00:10:00" do
     berlin_clock.setTime "00:10:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4YYOOOOOOOOO 5OOOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO YYOOOOOOOOO OOOO"
   end
 
-  it "should return 1Y 2OOOO 3OOOO 4YYROOOOOOOO 5OOOO when input is 00:15:00" do
+  it "should return Y OOOO OOOO YYROOOOOOOO OOOO when input is 00:15:00" do
     berlin_clock.setTime "00:15:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4YYROOOOOOOO 5OOOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO YYROOOOOOOO OOOO"
   end
   
-  it "should return 1Y 2OOOO 3OOOO 4YYROOOOOOOO 5YYOO when input is 00:17:00" do
+  it "should return Y OOOO OOOO YYROOOOOOOO YYOO when input is 00:17:00" do
     berlin_clock.setTime "00:17:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4YYROOOOOOOO 5YYOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO YYROOOOOOOO YYOO"
   end
   
-  it "should return 1Y 2OOOO 3OOOO 4YYRYYROOOOO 5OOOO when input is 00:37:00" do
+  it "should return Y OOOO OOOO YYRYYROOOOO OOOO when input is 00:37:00" do
     berlin_clock.setTime "00:37:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4YYRYYRYOOOO 5YYOO"
+    expect berlin_clock.display.should eq "Y OOOO OOOO YYRYYRYOOOO YYOO"
   end
   
-  it "should return 1Y 2OOOO 3YOOO 4YYRYYROOOOO 5OOOO when input is 01:37:00" do
+  it "should return Y OOOO YOOO YYRYYROOOOO OOOO when input is 01:37:00" do
     berlin_clock.setTime "01:37:00"
-    expect berlin_clock.display.should eq "1Y 2OOOO 3YOOO 4YYRYYRYOOOO 5YYOO"
+    expect berlin_clock.display.should eq "Y OOOO YOOO YYRYYRYOOOO YYOO"
   end
 
-  it "should return 1Y 2YOOO 3OOOO 4OOOOOOOOOOO 5OOOO when input is 05:00:00" do
+  it "should return Y YOOO OOOO OOOOOOOOOOO OOOO when input is 05:00:00" do
     berlin_clock.setTime "05:00:00"
-    expect berlin_clock.display.should eq "1Y 2YOOO 3OOOO 4OOOOOOOOOOO 5OOOO"
+    expect berlin_clock.display.should eq "Y YOOO OOOO OOOOOOOOOOO OOOO"
   end
 
-  it "should return 1O 2YYYY 3YYYO 4YYRYYRYYRYY 5YYYY when input is 23:59:59" do
+  it "should return O YYYY YYYO YYRYYRYYRYY YYYY when input is 23:59:59" do
     berlin_clock.setTime "23:59:59"
-    expect berlin_clock.display.should eq "1O 2YYYY 3YYYO 4YYRYYRYYRYY 5YYYY"
+    expect berlin_clock.display.should eq "O YYYY YYYO YYRYYRYYRYY YYYY"
   end
 end

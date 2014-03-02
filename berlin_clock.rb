8b5d84ea670ab3ff181a6836f1d1aa3aad08354f
White Rabbit <rabbit@archer.local>
2014-03-02 14:33:15 +0700

    Add test 1 second past midnight

--- berlin_clock.rb
+++ berlin_clock.rb
@@ -1,8 +1,13 @@
 class BerlinClock
   def setTime time
+    @time = time
   end
 
   def display
-    "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO" 
+    if @time == '00:00:00'
+      "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO" 
+    elsif @time == '00:00:01'
+      "1O 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO" 
+    end
   end
 end
--- spec/berlin_clock_spec.rb
+++ spec/berlin_clock_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
+
 describe "Berlin Clock" do
+
   let (:berlin_clock) { BerlinClock.new }
+
   it "should return 1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO when input is 00:00:00" do
     berlin_clock.setTime "00:00:00"
     expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO"
+  end
+
+  it "should return 1O 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO when input is 00:00:01" do
+    berlin_clock.setTime "00:00:01"
+    expect berlin_clock.display.should eq "1O 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO"
   end
 end

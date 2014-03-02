cd4588810a51c432bd1bed0a705cfbb2fd401bbe
White Rabbit <rabbit@archer.local>
2014-03-02 14:25:50 +0700

    Refactor the code

--- spec/berlin_clock_spec.rb
+++ spec/berlin_clock_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 describe "Berlin Clock" do
+  let (:berlin_clock) { BerlinClock.new }
   it "should return 1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO when input is 00:00:00" do
-    berlin_clock = BerlinClock.new
-    berlin_clock.setTime("00:00:00")
-    expect(berlin_clock.display().should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO") 
+    berlin_clock.setTime "00:00:00"
+    expect berlin_clock.display.should eq "1Y 2OOOO 3OOOO 4OOOOOOOOOOO 5OOOO"
   end
 end

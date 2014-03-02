d3dd6b055045cd5d16c3c913a4897f321b5ca1e6
Chanathip Srithanrat <chanathip@tree.local>
2014-03-02 23:26:52 +0700

    Refactor the code

--- berlin_clock.rb
+++ berlin_clock.rb
@@ -1,8 +1,6 @@
 class BerlinClock
 
-  def setTime time
-    @time = time
-  end
+  attr_writer :time
 
   def display
     "#{get_second} #{get_hour} #{get_minute}"
@@ -10,8 +8,8 @@ class BerlinClock
 
   def run
     while true
-      @time = Time.now.strftime("%H:%M:%S")
-      print @time + " "
+      @time = Time.now.strftime('%H:%M:%S')
+      print @time + ' '
       puts self.display
       sleep 1
     end
@@ -20,19 +18,19 @@ class BerlinClock
   private
   def get_second
     second = @time[6..7].to_i
-    sec = (second % 2 == 1 ? "O" : "Y")
+    sec = (second % 2 == 1 ? 'O' : 'Y')
   end
 
   def get_minute
     minute = @time[3..4].to_i
     min_div_five = minute / 5
     min_mod_five = minute % 5
-    min1 = ""
+    min1 = ''
     (1..(min_div_five)).each do |i|
-      min1 += i % 3 != 0 ? "Y" : "R"
+      min1 += i % 3 != 0 ? 'Y' : 'R'
     end
-    min1 += "O" * (11 - min_div_five)
-    min2 = "Y" * (min_mod_five) + "O" * (4 - min_mod_five)
+    min1 += 'O' * (11 - min_div_five)
+    min2 = 'Y' * (min_mod_five) + 'O' * (4 - min_mod_five)
     "#{min1} #{min2}"
   end
 
@@ -40,8 +38,8 @@ class BerlinClock
     hour = @time[0..1].to_i
     hour_div_five = hour / 5
     hour_mod_five = hour % 5
-    hour1 = "Y" * (hour_div_five) + "O" * (4 - hour_div_five) 
-    hour2 = "Y" * (hour_mod_five) + "O" * (4 - hour_mod_five) 
+    hour1 = 'Y' * (hour_div_five) + 'O' * (4 - hour_div_five) 
+    hour2 = 'Y' * (hour_mod_five) + 'O' * (4 - hour_mod_five) 
     "#{hour1} #{hour2}"
   end
 
--- spec/berlin_clock_spec.rb
+++ spec/berlin_clock_spec.rb
@@ -5,72 +5,72 @@ describe "Berlin Clock" do
   let (:berlin_clock) { BerlinClock.new }
 
   it "should return Y OOOO OOOO OOOOOOOOOOO OOOO when input is 00:00:00" do
-    berlin_clock.setTime "00:00:00"
+    berlin_clock.time = "00:00:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO OOOO"
   end
 
   it "should return O OOOO OOOO OOOOOOOOOOO OOOO when input is 00:00:01" do
-    berlin_clock.setTime "00:00:01"
+    berlin_clock.time = "00:00:01"
     expect berlin_clock.display.should eq "O OOOO OOOO OOOOOOOOOOO OOOO"
   end
 
   it "should return Y OOOO OOOO OOOOOOOOOOO YOOO when input is 00:01:00" do
-    berlin_clock.setTime "00:01:00"
+    berlin_clock.time = "00:01:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO YOOO"
   end
 
   it "should return Y OOOO OOOO OOOOOOOOOOO YYYY when input is 00:04:00" do
-    berlin_clock.setTime "00:04:00"
+    berlin_clock.time = "00:04:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO YYYY"
   end
   
   it "should return Y OOOO OOOO OOOOOOOOOOO YYOO when input is 00:02:00" do
-    berlin_clock.setTime "00:02:00"
+    berlin_clock.time = "00:02:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO OOOOOOOOOOO YYOO"
   end
 
   it "should return Y OOOO OOOO YOOOOOOOOOO OOOO when input is 00:05:00" do
-    berlin_clock.setTime "00:05:00"
+    berlin_clock.time = "00:05:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO YOOOOOOOOOO OOOO"
   end
 
   it "should return Y OOOO OOOO YOOOOOOOOOO YYOO when input is 00:07:00" do
-    berlin_clock.setTime "00:07:00"
+    berlin_clock.time = "00:07:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO YOOOOOOOOOO YYOO"
   end
 
   it "should return Y OOOO OOOO YYOOOOOOOOO OOOO when input is 00:10:00" do
-    berlin_clock.setTime "00:10:00"
+    berlin_clock.time = "00:10:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO YYOOOOOOOOO OOOO"
   end
 
   it "should return Y OOOO OOOO YYROOOOOOOO OOOO when input is 00:15:00" do
-    berlin_clock.setTime "00:15:00"
+    berlin_clock.time = "00:15:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO YYROOOOOOOO OOOO"
   end
   
   it "should return Y OOOO OOOO YYROOOOOOOO YYOO when input is 00:17:00" do
-    berlin_clock.setTime "00:17:00"
+    berlin_clock.time = "00:17:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO YYROOOOOOOO YYOO"
   end
   
   it "should return Y OOOO OOOO YYRYYROOOOO OOOO when input is 00:37:00" do
-    berlin_clock.setTime "00:37:00"
+    berlin_clock.time = "00:37:00"
     expect berlin_clock.display.should eq "Y OOOO OOOO YYRYYRYOOOO YYOO"
   end
   
   it "should return Y OOOO YOOO YYRYYROOOOO OOOO when input is 01:37:00" do
-    berlin_clock.setTime "01:37:00"
+    berlin_clock.time = "01:37:00"
     expect berlin_clock.display.should eq "Y OOOO YOOO YYRYYRYOOOO YYOO"
   end
 
   it "should return Y YOOO OOOO OOOOOOOOOOO OOOO when input is 05:00:00" do
-    berlin_clock.setTime "05:00:00"
+    berlin_clock.time = "05:00:00"
     expect berlin_clock.display.should eq "Y YOOO OOOO OOOOOOOOOOO OOOO"
   end
 
   it "should return O YYYY YYYO YYRYYRYYRYY YYYY when input is 23:59:59" do
-    berlin_clock.setTime "23:59:59"
+    berlin_clock.time = "23:59:59"
     expect berlin_clock.display.should eq "O YYYY YYYO YYRYYRYYRYY YYYY"
   end
 end
